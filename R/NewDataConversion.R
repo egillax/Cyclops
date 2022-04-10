2648cad2394209e230cad9c7ecc70dc7b1937947
Egill Fridgeirsson <egillax@gmail.com>
2022-04-10 18:12:16 +0200

    add arrow support

--- R/NewDataConversion.R
+++ R/NewDataConversion.R
@@ -115,7 +115,7 @@ convertToCyclopsData.data.frame <- function(outcomes,
 
     if (modelType == "lr" | modelType == "pr") {
         outcomes$stratumId <- NULL
-        covariates$stratumId <- NULL
+        # covariates$stratumId <- NULL
     }
     if ((modelType == "cox" | modelType == "fgr") & !"stratumId" %in% colnames(outcomes)) {
         outcomes$stratumId <- 0
@@ -380,3 +380,155 @@ convertToCyclopsData.tbl_dbi <- function(outcomes,
 
     return(dataPtr)
 }
+
+
+#' @describeIn convertToCyclopsData Convert data from two \code{Dataset} tables
+#' @export
+convertToCyclopsData.Dataset <- function(outcomes,
+                                         covariates,
+                                         modelType = "lr",
+                                         addIntercept = TRUE,
+                                         checkSorting = NULL,
+                                         checkRowIds = TRUE,
+                                         normalize = NULL,
+                                         quiet = FALSE,
+                                         floatingPoint = 64) {
+    class(outcomes) <- "data.frame"
+    if (!is.null(checkSorting))
+        warning("The 'checkSorting' argument has been deprecated. Sorting is now always checked")
+
+    if ((modelType == "clr" | modelType == "cpr") & addIntercept) {
+        if (!quiet) {
+            warning("Intercepts are not allowed in conditional models, removing intercept", call. = FALSE)
+        }
+        addIntercept = FALSE
+    }
+
+    if (modelType == "pr" | modelType == "cpr") {
+        if (any(outcomes$time <= 0)) {
+            stop("time cannot be non-positive", call. = FALSE)
+        }
+    }
+
+    providedNoStrata <- !"stratumId" %in% colnames(outcomes)
+
+    if (modelType == "cox" | modelType == "fgr") {
+        if (providedNoStrata) {
+            outcomes <- outcomes %>%
+                mutate(stratumId = 0)
+            covariates <- covariates %>%
+                mutate(stratumId = 0)
+        }
+    }
+
+    if (checkRowIds) {
+        covariateRowIds <- covariates %>%
+            distinct(.data$rowId) %>%
+            pull()
+        outcomeRowIds <- select(outcomes, .data$rowId) %>%
+            pull()
+        mapping <- match(covariateRowIds, outcomeRowIds)
+        if (any(is.na(mapping))) {
+            if (!quiet) {
+                writeLines("Removing covariate values with rowIds that are not in outcomes")
+            }
+            covariates <- covariates %>%
+                filter(.data$rowId %in% outcomeRowIds)
+        }
+    }
+
+    # Sorting should be last, as other operations may change ordering.
+    # Also, should always explicitly define sorting, else not guaranteed.]
+    if (modelType == "lr" | modelType == "pr") {
+        outcomes <- outcomes %>%
+            arrange(.data$rowId)
+
+        covariates <- covariates %>%
+            arrange(.data$covariateId, .data$rowId)
+    }
+
+    if (modelType == "clr" | modelType == "cpr") {
+        outcomes <- outcomes %>%
+            arrange(.data$stratumId, .data$rowId)
+
+        covariates <- covariates %>%
+            arrange(.data$covariateId, .data$stratumId, .data$rowId)
+    }
+
+    if (modelType == "cox" | modelType == "fgr") {
+
+        if (modelType == "cox" &
+            (select(outcomes, .data$y) %>% distinct() %>% count() %>% collect() > 2)) {
+            stop("Cox model only accepts one outcome type")
+        }
+
+        outcomes <- outcomes %>%
+            arrange(.data$stratumId, desc(.data$time), .data$y, .data$rowId)
+        if (!"time" %in% colnames(covariates)) {
+            covariates <- covariates %>%
+                inner_join(select(outcomes, .data$rowId, .data$time, .data$y), by = "rowId")
+        }
+        covariates <- covariates %>%
+            arrange(.data$covariateId, .data$stratumId, desc(.data$time), .data$y, .data$rowId)
+    }
+
+    dataPtr <- createSqlCyclopsData(modelType = modelType, floatingPoint = floatingPoint)
+
+    outcomes <- collect(outcomes)
+    if (modelType == "lr" | modelType == "pr") {
+        outcomes$stratumId <- NULL
+    }
+
+    loadNewSqlCyclopsDataY(object = dataPtr,
+                           stratumId = if ("stratumId" %in% colnames(outcomes)) outcomes$stratumId else NULL,
+                           rowId = outcomes$rowId,
+                           y = outcomes$y,
+                           time = if ("time" %in% colnames(outcomes)) outcomes$time else NULL)
+
+    if (addIntercept & (modelType != "cox" & modelType != "fgr")) {
+        loadNewSqlCyclopsDataX(dataPtr, 0, NULL, NULL, name = "(Intercept)")
+    }
+
+    loadCovariates <- function(batch) {
+        batch <- as.data.frame(batch)
+        covarNames <- unique(batch$covariateId)
+        loadNewSqlCyclopsDataMultipleX(object = dataPtr,
+                                       covariateId = batch$covariateId,
+                                       rowId = batch$rowId,
+                                       covariateValue = batch$covariateValue,
+                                       name = covarNames,
+                                       append = TRUE)
+    }
+    arrow::map_batches(covariates, loadCovariates)
+
+
+    if (modelType == "pr" || modelType == "cpr")
+        finalizeSqlCyclopsData(dataPtr, useOffsetCovariate = -1)
+
+    if (!is.null(normalize)) {
+        .normalizeCovariates(dataPtr, normalize)
+    }
+
+    if ("weights" %in% colnames(outcomes)) {
+        dataPtr$weights <- outcomes %>% pull(.data$weights)
+    } else {
+        dataPtr$weights <- NULL
+    }
+
+    if ("censorWeights" %in% colnames(outcomes)) {
+        dataPtr$censorWeights <- outcomes %>% pull(.data$censorWeights)
+    } else {
+        if (modelType == "fgr") {
+            dataPtr$censorWeights <- getFineGrayWeights(
+                outcomes %>% pull(.data$time),
+                outcomes %>% pull(.data$y)
+            )$weights
+            writeLines("Generating censoring weights")
+        } else {
+            dataPtr$censorWeights <- NULL
+        }
+    }
+
+    return(dataPtr)
+}
+
